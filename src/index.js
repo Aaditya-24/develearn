88228db1d449339dbd3995d29213adcc8c74250e
Aditya Sharma <aditya.sharma@develearn.in>
2021-12-03 14:54:06 +0530

    Pushed Enviroment Variable Connections

--- src/index.js
+++ src/index.js
@@ -18,10 +18,12 @@ const server = new ApolloServer({
   },
 });
 
-server.listen().then(() => {
-  console.log(`
-    🚀  Server is running!
-    🔉  Listening on port 4000
-    📭  Query at https://studio.apollographql.com/dev
-  `);
-});
+// server.listen().then(() => {
+//   console.log(`
+//     🚀  Server is running!
+//     🔉  Listening on port 4000
+//     📭  Query at https://studio.apollographql.com/dev
+//   `);
+// });
+
+await server.listen({port: process.env.PORT || 4000});
